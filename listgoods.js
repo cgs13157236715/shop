a650d3e9c227cc4c2a14f8fa354c98372a07c18c
cgs13157236715 <cgs13157236715@163.com>
2019-10-10 12:00:55 +0800

    sdfsd

--- listgoods.js
+++ listgoods.js
@@ -1,10 +1,12 @@
 
 const http = require('http');
 const ysql= require('mysql');
-ttp = require('http');
+ttp = require('h88tp');
 const myql= rquire('mysql');
 ttp = rquire('http');
 const mysql= requie('mysql');
+
+
 ttp = require('http');
 const mysql= require('mysql');
 234234324
