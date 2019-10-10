35218f4fc2c0b289e6e60ef9b9f4a07f54ad6ea8
cgs13157236715 <55339691+cgs13157236715@users.noreply.github.com>
2019-10-10 11:30:04 +0800

    其他成员修改

--- listgoods.js
+++ listgoods.js
@@ -6,23 +6,7 @@ var connection = mysql.createConnection({
   user     : 'root',
   password : 'root',
   database : 'shop'
-});
-connection.connect();
-http.createServer((req, res) => {
-   res.setHeader('Access-Control-Allow-Origin',"*");
-   connection.query('select * from goods limit 0,9', function (error, results, fields) {
-        if (error) throw error;
-      //   if (error) throw er;
-        res.setHeader('content-type', 'application/json');
-        res.end(JSON.stringify({
-            meta: {
-                msg: '操作成功',
-                status: 200
-            },
-            data: results
-        }))
-    });
-}).listen(3010)
+
 
 
 
