4785d9d722adb78764805c94e3d71d88c893c5d5
cgs13157236715 <55339691+cgs13157236715@users.noreply.github.com>
2019-10-10 11:43:04 +0800

    Update listgoods.js

--- listgoods.js
+++ listgoods.js
@@ -1,13 +1,6 @@
 
 const http = require('http');
 const mysql= require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  database : 'shop'
-});
-connection.connect();
 
 
 
